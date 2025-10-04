a0eb933fa33e0daa28b2b6628913c1ddea102fc2
Douglas Kosovic <doug@uq.edu.au>
2025-10-04 23:20:03 +1000

    Update CFrameHelp.java files to use modernised events

--- src/OneDOF/Prog4/Damped/CFrameHelp.java
+++ src/OneDOF/Prog4/Damped/CFrameHelp.java
@@ -7,20 +7,24 @@ import java.awt.Polygon;
 import java.util.StringTokenizer;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.Timer;
 
 @SuppressWarnings("serial")
-class CFrameHelp extends CFrame implements ActionListener {
+public class CFrameHelp extends CFrame implements ActionListener, MouseListener {
    Timer mTimer;
    boolean mShowtime;
    public String mMessage;
    public double mDelay = 1.5;
    public Point mArrowHead;
 
-   public CFrameHelp(CFramePanel thePanel, int xx, int yy, int ww, int hh, String message) {
-      super(thePanel, xx, yy, ww, hh);
-      this.mMessage = message;
-      this.DefaultArrowPos();
+   public CFrameHelp() {
+      super(288, 324, 1, new String[]{"OK"});
+      this.mShowtime = false;
+      this.mTimer = null;
+      this.mDelay = 3.0;
+      this.addMouseListener(this);
    }
 
    public void DefaultArrowPos() {
@@ -101,7 +105,8 @@ class CFrameHelp extends CFrame implements ActionListener {
       }
    }
 
-   public boolean MouseEvent(int code, boolean prevHit) {
+   @Override
+   public void mousePressed(MouseEvent e) {
       super.mWasHit = false;
       if (this.mShowtime) {
          this.repaint();
@@ -109,15 +114,39 @@ class CFrameHelp extends CFrame implements ActionListener {
 
       this.mShowtime = false;
       this.mTimer = null;
-      if (code != 3) {
-         return false;
-      } else if (!this.contains(super.mFramePanel.mThisPt.x, super.mFramePanel.mThisPt.y)) {
-         return false;
-      } else {
-         this.mTimer = new Timer((int)(this.mDelay * 1000), this);
-         this.mTimer.start();
-         return true;
+
+      // Check if this is a left mouse button click (button 1)
+      if (e.getButton() != MouseEvent.BUTTON1) {
+         return;
       }
+
+      // Check if click is within component bounds
+      if (!this.contains(e.getX(), e.getY())) {
+         return;
+      }
+
+      this.mTimer = new Timer((int)(this.mDelay * 1000), this);
+      this.mTimer.start();
+   }
+
+   @Override
+   public void mouseClicked(MouseEvent e) {
+      // Not used
+   }
+
+   @Override
+   public void mouseReleased(MouseEvent e) {
+      // Not used
+   }
+
+   @Override
+   public void mouseEntered(MouseEvent e) {
+      // Not used
+   }
+
+   @Override
+   public void mouseExited(MouseEvent e) {
+      // Not used
    }
 
    public void actionPerformed(ActionEvent e) {
